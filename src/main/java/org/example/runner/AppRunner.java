5ed1ba69b1ce016467c226d01596fd600d14a206
Pavel Drozhzhachih <pavel.drozhzhachih@gmail.com>
2025-12-21 18:02:45 +0300

    homework 6 Spring Shell

--- src/main/java/org/example/runner/AppRunner.java
+++ src/main/java/org/example/runner/AppRunner.java
@@ -3,9 +3,7 @@ package org.example.runner;
 import org.example.service.TestingService;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
 
-@Component
 public class AppRunner implements ApplicationRunner {
 
     private final TestingService testingService;
